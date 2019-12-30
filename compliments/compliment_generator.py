53efa6703c89000bc92981d66f97d744ede45fb2
Daniel Bücheler <dabch@posteo.de>
2019-12-31 00:20:31 +0100

    update compliment generator for new format
    also replaces umlauts with their respective hex codes

--- compliments/compliment_generator.py
+++ compliments/compliment_generator.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import sys
 
-STRING_DECL = "const char c{}_{}[] PROGMEM = \"{}\";"
-#STRUCT_DECL = "const compliment_t c{} PROGMEM = {{{}, c{}a}};"
-ARRAY_DECL = "const char* const c{}a[] = {{{}}};"
-#ARRAY_DECL = "const char* const c{}a[] PROGMEM = {{{}}};"
-ARRAY_ELEMENT_DECL = "c{}_{}"
+STRING_DECL = "const char c{}[] PROGMEM = \"\\x05\"\"{}\\x04\";"
+ARRAY_DECL = "const char *const compliments_progmem[] = {{{}}};"
+COUNT_DECL = "const int COMPLIMENT_COUNT = {};"
 
-MAP_DECL = "const compliment_t compliment_table[] = {{{}}};"
-#MAP_DECL = "const compliment_t compliment_table[] PROGMEM = {{{}}};"
-MAP_ELEMENT_DECL = "{{{}, c{}a}}"
+DEBUG = False
 
 def main():
-    if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} [line length]")
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} [line length] [max line count]")
         return
     line_len = int(sys.argv[1])
+    line_count = int(sys.argv[2])
     if line_len <= 0:
         print("Invalid line length")
-        print(f"Usage: {sys.argv[0]} [line length]")
+        print(f"Usage: {sys.argv[0]} [line length] [max line count]")
+        return
+    if line_count <= 0:
+        print("Invalid line count")
+        print(f"Usage: {sys.argv[0]} [line length] [max line count]")
         return
 
     print("// ---------------- BEGIN GENERATED COMPLIMENTS -------------")
@@ -34,48 +36,57 @@ def main():
             compliment = input()
         except EOFError:
             break
-
-        line = 0
-        words = compliment.split(" ")
-        current = ""
+        line_idx = 0
+        lines = [""]
+        if len(compliment.strip()) <= 0 or compliment.startswith('#'):
+            continue
+        words = compliment.strip().split(" ")
         while len(words) > 0:
-            if (len(current) + len(words[0])) <= line_len:
-                current += words[0] + " "
+            if DEBUG:
+                print("===")
+                print(words[0])
+                print(line_idx)
+                print(lines)
+                print(lines[line_idx])
+
+            if (len(lines[line_idx]) + len(words[0])) <= line_len:
+                lines[line_idx] += words[0] + " " 
                 words = words[1:]
             elif len(words[0]) > line_len:
-                part_1 = words[0][:line_len - len(current)]
-                part_2 = words[0][line_len - len(current):]
-                current += part_1 + " "
+                part_1 = words[0][:line_len - len(lines[line_idx]) - 1]
+                part_2 = words[0][line_len - len(lines[line_idx]) - 1:]
+                lines[line_idx] += part_1 + "- "
                 words = [part_2] + words[1:]
-                print(STRING_DECL.format(count, line, current.strip()))
-                line += 1
-                current = ""
+                line_idx += 1
+                lines.append("")
             else:
-                print(STRING_DECL.format(count, line, current.strip()))
-                line += 1
-                current = ""
-        print(STRING_DECL.format(count, line, current.strip()))
-        line += 1
+                lines[line_idx] = lines[line_idx].strip()
+                lines.append("")
+                line_idx += 1
 
-        array_elements_list = [ARRAY_ELEMENT_DECL.format(count, i) for i in range(line)]
-        array_elements = ", ".join(array_elements_list)
-        
-        print(ARRAY_DECL.format(count, array_elements))
-        #struct_elems = struct_elems.append(MAP_ELEMENT_DECL.format(line, count))
-        struct_elems_list.append(MAP_ELEMENT_DECL.format(line, count))
+        if len(lines) > line_count:
+            print(f"ERROR: compliment too long ({len(lines)} lines), please adjust manually: \"{compliment}\"", file=sys.stderr)
+        compliment = "\\n".join(lines).strip()
+        if len(lines) <= 4:
+            compliment = '\\n' + compliment
+        if len(lines) <= 2:
+            compliment = '\\n' + compliment
+
+        compliment = compliment.replace("ö", "\\x94\"\"")
+        compliment = compliment.replace("ü", "\\x81\"\"")
+        compliment = compliment.replace("ä", "\\x84\"\"")
+        compliment = compliment.replace("Ä", "\\x8e\"\"")
+        compliment = compliment.replace("Ö", "\\x99\"\"")
+        compliment = compliment.replace("Ü", "\\x9a\"\"")
+        compliment = compliment.replace("ß", "\\xe0\"\"")
+
+        print(STRING_DECL.format(count, compliment))
         count += 1
-        print()
 
-    if count <= 0:
-        return
+    print(ARRAY_DECL.format(', '.join([f"c{i}" for i in range(count)])))
 
-    #table_elements_list = [MAP_ELEMENT_DECL.format(i) for i in range(count)]
-    struct_elems = ", ".join(struct_elems_list)
-    print(MAP_DECL.format(struct_elems))
-
-    print(f"const int COMPLIMENT_COUNT = {count};")
-            
-    print("// ---------------- END GENERATED COMPLIMENTS -------------")
+    print(COUNT_DECL.format(count))
+                
 
 if __name__ == "__main__":
     main()
